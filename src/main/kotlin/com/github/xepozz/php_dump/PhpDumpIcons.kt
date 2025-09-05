f925a39d0427026997f95024730bf0f3f60113f8
Dmitriy Derepko <xepozz@list.ru>
2025-09-05 16:23:20 +0400

    fix: smaller icons

--- src/main/kotlin/com/github/xepozz/php_dump/PhpDumpIcons.kt
+++ src/main/kotlin/com/github/xepozz/php_dump/PhpDumpIcons.kt
@@ -4,15 +4,15 @@ import com.intellij.openapi.util.IconLoader
 
 object PhpDumpIcons {
     @JvmStatic
-    val POT = IconLoader.getIcon("/icons/pot.svg", PhpDumpIcons::class.java)
+    val POT = IconLoader.getIcon("/icons/pot.svg", javaClass)
     @JvmStatic
-    val POT_DARK = IconLoader.getIcon("/icons/pot_dark.svg", PhpDumpIcons::class.java)
+    val POT_DARK = IconLoader.getIcon("/icons/pot_dark.svg", javaClass)
     @JvmStatic
-    val RERUN_AUTOMATICALLY = IconLoader.getIcon("/icons/rerunAutomatically/rerunAutomatically.svg", PhpDumpIcons::class.java)
+    val RERUN_AUTOMATICALLY = IconLoader.getIcon("/icons/rerunAutomatically/rerunAutomatically.svg", javaClass)
     @JvmStatic
-    val RESTART_STOP = IconLoader.getIcon("/icons/restartStop/restartStop.svg", PhpDumpIcons::class.java)
+    val RESTART_STOP = IconLoader.getIcon("/icons/restartStop/restartStop.svg", javaClass)
     @JvmStatic
-    val SHOW_AS_TREE = IconLoader.getIcon("/icons/showAsTree/showAsTree.svg", PhpDumpIcons::class.java)
+    val SHOW_AS_TREE = IconLoader.getIcon("/icons/showAsTree/showAsTree.svg", javaClass)
     @JvmStatic
-    val TEXT = IconLoader.getIcon("/icons/text/text.svg", PhpDumpIcons::class.java)
+    val TEXT = IconLoader.getIcon("/icons/text/text.svg", javaClass)
 }
